f4d5b1934e3bf59326653f4cd7c9d9aad678c4f9
Thanh Ky Quan <thanhquanky@gatech.edu>
2015-02-13 03:15:52 -0500

    Show question by question

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 var ngQuizApp = angular.module('ngQuizApp', []);
 
-ngQuizApp.controller('QuestionCtrl', function($scope) {
-    $scope.questions = ['Where is Vietnam', 'Who is the first president of Vietnam?'];
+ngQuizApp
+.controller('MainCtrl', function($scope) {
+    $scope.score = 0;
+    $scope.questionIndex = 0;
+    
+    $scope.quizPackage = [
+        {   
+            question: 'Where is Vietnam?',
+            choices: [
+                'Asia',
+                'Africa',
+                'Europe',
+                'North America'
+            ]
+        },
+        {
+            question: 'Who is the first president of Vietnam?',
+            choices: [
+                'Do Muoi',
+                'Ho Chi Minh',
+                'Vo Van Kiet',
+                'Truong Tan Sang'
+            ]
+        }
+    ];
+    
+    $scope.nextQuestion = function() {
+        $scope.questionIndex++;
+    }
 });
